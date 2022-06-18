9ddb64fd4b0802d765ecbeb0b3825605af058924
Marco Gongora <marco.gongora@spreetail.com>
2022-06-17 23:17:20 -0600

    Added FinancialChat.Infrastructure.Test project and FinancialChat.Bot project

---
+++ FinancialChat.Infrastructure.Test/DB/DBContextInitializer.cs
@@ -0,0 +1,42 @@
+﻿
+using FinancialChat.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancialChat.Infrastructure.Test.DB
+{
+  internal static class DBContextInitializer
+  {
+    public static Infrastructure.DB.DbContext GetContext()
+    {
+
+    DbContextOptions<Infrastructure.DB.DbContext> options;
+    var builder = new DbContextOptionsBuilder<Infrastructure.DB.DbContext>();
+    builder.UseInMemoryDatabase("chat-app");
+      options = builder.Options;
+      var context = new Infrastructure.DB.DbContext(options);
+      context.AddRange(GetChatRooms());
+      context.AddRange(GetChatMessages());
+      context.SaveChanges();
+      return context;
+    }
+
+    private static IEnumerable<ChatMessage> GetChatMessages()
+    {
+      return new List<ChatMessage>
+      {
+        new ChatMessage {Message =  "Hello World", Date = new DateTime(), RoomName = "room-1", User = ""},
+        new ChatMessage {Message =  "Hello Room 1", Date = new DateTime(), RoomName = "room-1", User = ""},
+        new ChatMessage {Message =  "Hello Room 2", Date = new DateTime(), RoomName = "room-2", User = ""}
+      };
+    }
+
+    private static IEnumerable<ChatRoom> GetChatRooms()
+    {
+      return new List<ChatRoom>
+      {
+        new ChatRoom {Name = "room-1"},        
+        new ChatRoom {Name = "room-2"}
+      };
+    }
+  }
+}
---
+++ FinancialChat.Infrastructure.Test/Repositories/ChatMessageRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using FinancialChat.Core.Entities;
+using FinancialChat.Infrastructure.DB;
+using FinancialChat.Infrastructure.Test.DB;
+
+namespace FinancialChat.Infrastructure.Repositories.Tests
+{
+  [TestClass()]
+  public class ChatMessageRepositoryTests
+  {
+    private static DbContext _ctx;
+    private static ChatMessageRepository _repository;
+
+    [ClassInitialize]
+    public static void Initialize(TestContext testContext)
+    {
+      _ctx = DBContextInitializer.GetContext();
+      _repository = new ChatMessageRepository(_ctx);
+    }
+
+
+    [TestMethod()]
+    public async Task DeleteTestAsync()
+    {
+      var id = 3;
+      var count = await _repository.Delete(id);
+      Assert.AreEqual(1, count);
+      Assert.IsNull(_ctx.ChatMessages.FirstOrDefault(x => x.Id == id));
+    }
+
+    [TestMethod()]
+    public async Task GetByRoomTest()
+    {
+      var roomName = "room-1";
+      var expectedMessages = new string[] { "Hello World", "Hello Room 1" };
+      var result = await _repository.GetByRoom(roomName);
+      Assert.IsNotNull(result);
+      var messages = result.Select(x => x.Message).ToArray();
+      Assert.AreEqual(2, messages.Count());
+      CollectionAssert.AreEqual(expectedMessages, messages);
+    }
+
+    [TestMethod()]
+    public async Task InsertTest()
+    {
+      var message = new ChatMessage
+      {
+        Date = DateTime.Now,
+        Message = "Hello test",
+        RoomName = "room-2",
+        User = "test"
+      };
+      var data = await _repository.Insert(message);
+      Assert.AreEqual(4, data.Id);
+    }
+
+    [TestMethod()]
+    public async Task UpdateTest()
+    {
+      var id = 4;
+      var message = new ChatMessage
+      {
+        Id = id,
+        Date = DateTime.Now,
+        Message = "Hello test modified",
+        RoomName = "room-2",
+        User = "test-modified"
+      };
+      var count = await _repository.Update(message);
+      Assert.AreEqual(1, count);
+    }
+  }
+}
