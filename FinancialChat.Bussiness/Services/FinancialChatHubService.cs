30e2d92ffed507aade8794c4f9e6f651d06b9c9a
Marco Gongora <marco.gongora@spreetail.com>
2022-06-20 23:17:54 -0600

    Added FinancialChatHubService

---
+++ FinancialChat.Bussiness/Services/FinancialChatHubService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace FinancialChat.Core.Services
+{
+  public class FinancialChatHubService
+  {
+    private HubConnection? hubConnection;
+
+    public async Task<HubConnection> Connect(string url)
+    {
+      hubConnection = new HubConnectionBuilder()
+            .WithUrl(url)
+            .WithAutomaticReconnect()
+            .Build();
+      await hubConnection.StartAsync();
+      return hubConnection;
+    }
+
+    public async Task JoinGroup(string group)
+    {
+      if(hubConnection != null)
+      {
+        await hubConnection.InvokeAsync("AddToGroup", group);
+      }
+    }
+
+    public async Task SendMessage(string group, string user, string message)
+    {
+      if (hubConnection != null)
+      {
+        await hubConnection.InvokeAsync("SendMessage", group, user, message);
+      }
+    }
+
+    public async Task LeaveGroup(string group)
+    {
+      if (hubConnection != null)
+      {
+        await hubConnection.InvokeAsync("LeaveGroup", group);
+      }
+    }
+  }
+}
